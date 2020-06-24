a809f9333a22903205df83f423ac8bd04c3f5cc0
gityibo <Qzaqmju@qq.com>
2020-06-25 00:03:34 +0800

    更新

--- src/main/java/org/yin/tools/config/WebConfig.java
+++ src/main/java/org/yin/tools/config/WebConfig.java
@@ -2,13 +2,11 @@ package org.yin.tools.config;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.nio.charset.Charset;
 import java.util.List;
